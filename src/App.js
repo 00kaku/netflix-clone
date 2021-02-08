88d7d678b450fb5b45b980b4706797150d2336b7
Kaku <you@example.com>
2021-02-08 20:44:17 +0530

    Rows Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
+import './App.css'
+import Row from './Row'
+import requests from './requests'
 
 function App() {
   return (
     <div className="App">
-          NETFLIX CLONE
+         {/*BNANNER */}
+         {/*NAVBAR*/}
+
+          <Row title="NETFLIX ORIGNALS" 
+          fetchUrl={requests.fetchNetflixOrignals}
+          isLargeRow/>
+
+          <Row title="Trending Now" 
+          fetchUrl={requests.fetchTrending}
+          />
+
+
+          <Row title="Top Rated" 
+          fetchUrl={requests.fetchTopRated}/>
+
+
+          <Row title="Action Movies" 
+          fetchUrl={requests.fetchActionnMovies}/>
+
+
+          <Row title="Horror Movies" 
+          fetchUrl={requests.fetchHorrorMovies}/>
+
+          <Row title="Comedy Movies" 
+          fetchUrl={requests.fetchComedyMovies}/>
+
+          <Row title="Romance Movies" 
+          fetchUrl={requests.fetchRomanticnMovies}/>
+
+          <Row title="Documentaries" 
+          fetchUrl={requests.fetchDocumentaries}/>
+    
     </div>
   );
 }
