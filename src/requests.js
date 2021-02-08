60402f9d2b2795a865a5e4a89e5bf862ab155c23
akash sharma <22026768+00kaku@users.noreply.github.com>
2021-02-09 00:03:33 +0530

    Update requests.js

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY='bee5137ebb72928d7a908672a78a494f';
+const API_KEY='your api key here';
 
 const requests ={
 	fetchTrending  :      `/trending/all/week?api_key=${API_KEY}&language=en-US`,
